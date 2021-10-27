84e803023a08d5e0a444c34e12f4882a61aa515c
blugee <blugee>
2021-10-27 18:43:01 +0530

    changes for table filter

--- src/Components/DefaultTable/index.js
+++ src/Components/DefaultTable/index.js
@@ -1,30 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Table } from 'antd';
 import 'antd/dist/antd.css';
+const DefaultTable = (props) => {
 
-class DefaultTable extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
+    const [columns, setColumns] = useState([])
 
-        };
-
-    }
-
-    handleChange = (pagination, filters, sorter, extra) => {
-        console.log('params', pagination, filters, sorter, extra);
-        if (this.props.handleDataChange) {
-            this.props.handleDataChange(pagination, filters, sorter, extra)
+    useEffect(() => {
+        let array = []
+        for (let i = 0; i < props.columns.length; i++) {
+            let columnsData = props.columns[i]
+            if (columnsData.isFilter) {
+                let filters = []
+                for (let i = 0; i < props.data.length; i++) {
+                    let matchFound = filters.findIndex(item => item.value === props.data[i][columnsData.dataIndex])
+                    if (matchFound === -1) {
+                        filters.push({ value: props.data[i][columnsData.dataIndex], text: props.data[i][columnsData.dataIndex] })
+                    }
+                }
+                columnsData.filters = filters
+            }
+            array.push(columnsData)
         }
+        setColumns(array)
+    }, [props.columns])
 
+    
+    const handleChange = (pagination, filters, sorter, extra) => {
+        console.log('params', pagination, filters, sorter, extra);
+        if (props.handleDataChange) {
+            props.handleDataChange(pagination, filters, sorter, extra)
+        }
     }
 
-    render() {
-        const { columns, data, } = this.props
-        return (
-            <Table columns={columns} dataSource={data} onChange={this.handleChange} />
-        );
-    }
+
+    return (
+        <Table columns={columns} dataSource={props.data} onChange={handleChange} />
+    );
+
 }
 
 export default DefaultTable
--- src/Route/Dashboard/index.js
+++ src/Route/Dashboard/index.js
@@ -26,6 +26,30 @@ const data = [
         age: 32,
         address: 'London No. 2 Lake Park',
     },
+    {
+        key: '5',
+        name: 'John Brown',
+        age: 32,
+        address: 'New York No. 1 Lake Park',
+    },
+    {
+        key: '6',
+        name: 'Jim Green',
+        age: 42,
+        address: 'London No. 1 Lake Park',
+    },
+    {
+        key: '7',
+        name: 'Joe Black',
+        age: 32,
+        address: 'Sidney No. 1 Lake Park',
+    },
+    {
+        key: '8',
+        name: 'Jim Red',
+        age: 32,
+        address: 'London No. 2 Lake Park',
+    },
 ];
 
 class Dashboard extends React.Component {
@@ -41,7 +65,9 @@ class Dashboard extends React.Component {
             {
                 title: 'Name',
                 dataIndex: 'name',
+                isFilter:true,
                 defaultSortOrder: 'descend',
+                onFilter: (value, record) => record.name.indexOf(value) === 0,
                 sorter: (a, b) => this.sorter(a.name, b.name),
             },
             {
